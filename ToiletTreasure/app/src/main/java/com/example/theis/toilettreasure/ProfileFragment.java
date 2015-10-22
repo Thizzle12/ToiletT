d86efed921385c0dcea8dc870ed7e7095c0f2c99
Henrik <seje-smith@hotmail.com>
2015-10-22 15:07:45 +0200

    Fail

--- ToiletTreasure/app/src/main/java/com/example/theis/toilettreasure/ProfileFragment.java
+++ ToiletTreasure/app/src/main/java/com/example/theis/toilettreasure/ProfileFragment.java
@@ -5,13 +5,16 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.ListView;
 
 /**
  * Created by Theis on 12-10-2015.
  */
 
-public class ProfileFragment extends android.support.v4.app.Fragment {
+public class ProfileFragment extends android.support.v4.app.Fragment implements AdapterView.OnItemClickListener {
 
     ImageView profilePic;
 
@@ -21,7 +24,29 @@ public class ProfileFragment extends android.support.v4.app.Fragment {
         profilePic.setImageResource(R.drawable.henrikprofil);
 
 
-        return root;
+        String[] profile ={"Mine tanker", "Gemte tanker", "Bred ymer", "Instillinger"};
+
+        ArrayAdapter adapter = new ArrayAdapter(getActivity(), R.layout.profile_layout, R.id.profile_name, profile) {
+            @Override
+            public View getView(int position, View cachedView, ViewGroup parent) {
+                View view = super.getView(position, cachedView, parent);
+
+                return view;
+            }
+        };
+
+        ListView listView = new ListView(getActivity());
+        listView.setOnItemClickListener(this);
+
+
+
+        listView.setAdapter(adapter);
+        return listView;
+
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 
     }
 }
